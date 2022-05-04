ca55b5180eb4020957d81750ea3caa77129f15db
Arazz <91001726+Arazzq@users.noreply.github.com>
2022-05-04 23:00:41 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,101 +10,33 @@ logging.basicConfig(
 )
 LOGGER = logging.getLogger(__name__)
 
-api_id = int(os.environ.get("6775827"))
-api_hash = os.environ.get("05607067317e01a39ed1e5e1d21dce12")
-bot_token = os.environ.get("5235469670:AAHCiPoCz7dFHgSfB72PvMz_ZvVmdL3HUw0")
+api_id = int(os.environ.get("APP_ID"))
+api_hash = os.environ.get("APP_HASH")
+bot_token = os.environ.get("TOKEN")
 client = TelegramClient('client', api_id, api_hash).start(bot_token=bot_token)
 
 anlik_calisan = []
 
-@client.on(events.NewMessage(pattern='^(?i)/cancel'))
-async def cancel(event):
-  global anlik_calisan
-  anlik_calisan.remove(event.chat_id)
-
 
 @client.on(events.NewMessage(pattern="^/start$"))
 async def start(event):
-  await event.reply("**LoungeTagger Bot**, Grup veya kanaldaki neredeyse tüm üyelerden bahsedebilirim ★\nDaha fazla bilgi için **/help**'i tıklayın.",
+  await event.reply("**Aysun Bot**, ",
                     buttons=(
-                      [Button.url('🌟 Beni Bir Gruba Ekle', 'https://t.me/loungetaggerbot?startgroup=a'),
-                      Button.url('📣 Support', 'https://t.me/PassionisConversation'),
-                      Button.url('👨‍💻 Sahibim', 'https://t.me/ozuduqaqaw')]
+                      [Button.url('🌟 Məni Bir Qrupa Sal', 'https://t.me/Ayssunbot?startgroup=a'),
+                      Button.url('📣 Support', 'https://t.me/supmerlin'),
+                      Button.url('👨‍💻 Sahibim', 'https://t.me/Arazdi')]
                     ),
                     link_preview=False
                    )
 @client.on(events.NewMessage(pattern="^/help$"))
 async def help(event):
-  helptext = "**Loungetagger bot'un Yardım Menüsü**\n\nKomut: /all \n  Bu komutu, başkalarına bahsetmek istediğiniz metinle birlikte kullanabilirsiniz. \n`Örnek: /all Günaydın!`  \nBu komutu yanıt olarak kullanabilirsiniz. herhangi bir mesaj Bot, yanıtlanan iletiye kullanıcıları etiketleyecek"
+  helptext = "**Aysun bot'un Yardım Menyusu**\n\nƏmr: Hələ əmrlər hazır deyil təəssüf ki, \nSağol..."
   await event.reply(helptext,
                     buttons=(
-                      [Button.url('🌟 Beni Bir Gruba Ekle', 'https://t.me/loungetaggerbot?startgroup=a'),
-                       Button.url('📣 Support', 'https://t.me/PassionisConversation'),
-                      Button.url('👨‍💻 Sahibim', 'https://t.me/ozuduqaqaw')]
+                      [Button.url('🌟 Məni qrupa sal', 'https://t.me/Ayssunbot?startgroup=a'),
+                       Button.url('📣 Support', 'https://t.me/supmerlin'),
+                      Button.url('👨‍💻 Sahibim', 'https://t.me/Arazdi')]
                     ),
-                    link_preview=False
-                   )
-
-
-@client.on(events.NewMessage(pattern="^/all ?(.*)"))
-async def mentionall(event):
-  global anlik_calisan
-  if event.is_private:
-    return await event.respond("__Bu əmr gruplarda və kanallarda işlədilə bilər.!__")
-  
-  admins = []
-  async for admin in client.iter_participants(event.chat_id, filter=ChannelParticipantsAdmins):
-    admins.append(admin.id)
-  if not event.sender_id in admins:
-    return await event.respond("__Yalnızca adminlər hamısından deyilə bilir!__")
-  
-  if event.pattern_match.group(1):
-    mode = "text_on_cmd"
-    msg = event.pattern_match.group(1)
-  elif event.reply_to_msg_id:
-    mode = "text_on_reply"
-    msg = event.reply_to_msg_id
-    if msg == None:
-        return await event.respond("__Köhnə mesajlar üçün üyelerden bahsedemem! (gruba eklemeden önce gönderilen mesajlar)__")
-  elif event.pattern_match.group(1) and event.reply_to_msg_id:
-    return await event.respond("__Bana bir argüman ver!__")
-  else:
-    return await event.respond("__Bir mesajı yanıtlayın veya başkalarından bahsetmem için bana bir metin verin!__")
-    
-  if mode == "text_on_cmd":
-    anlik_calisan.append(event.chat_id)
-    usrnum = 0
-    usrtxt = ""
-    async for usr in client.iter_participants(event.chat_id):
-      usrnum += 1
-      usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}) "
-      if event.chat_id not in anlik_calisan:
-        await event.respond("İşlem Başarılı Bir Şekilde Durduruldu ❌")
-        return
-      if usrnum == 5:
-        await client.send_message(event.chat_id, f"{usrtxt}\n\n{msg}")
-        await asyncio.sleep(2)
-        usrnum = 0
-        usrtxt = ""
-        
-  
-  if mode == "text_on_reply":
-    anlik_calisan.append(event.chat_id)
- 
-    usrnum = 0
-    usrtxt = ""
-    async for usr in client.iter_participants(event.chat_id):
-      usrnum += 1
-      usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}) "
-      if event.chat_id not in anlik_calisan:
-        await event.respond("İşlem Başarılı Bir Şekilde Durduruldu ❌")
-        return
-      if usrnum == 5:
-        await client.send_message(event.chat_id, usrtxt, reply_to=msg)
-        await asyncio.sleep(2)
-        usrnum = 0
-        usrtxt = ""
-
-
-print(">> Bot Çox zor işliyir 🚀 @PassionisConversation məlumat alabilərsən <<")
+            
+print(">> Bot çalışıre ürəymm🚀 @ozuduqaqaw məlumat alabilərsən <<")
 client.run_until_disconnected()
